1d13ec69a68a747c623fbedb2f36ba1d70d4f365
anselbrandt <ansel@anselbrandt.com>
2020-09-14 20:58:28 -0400

    Add subscriptions

--- web/src/App.js
+++ web/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import { useQuery, gql } from "@apollo/client";
+import { useQuery, useSubscription, gql } from "@apollo/client";
 
 const query = gql`
   {
@@ -8,14 +8,24 @@ const query = gql`
   }
 `;
 
+const subscription = gql`
+  subscription {
+    time
+  }
+`;
+
 function App() {
-  const { loading, error, data } = useQuery(query);
+  const { loading: qloading, error: qerror, data: qdata } = useQuery(query);
+  const { loading: sloading, error: serror, data: sdata } = useSubscription(
+    subscription
+  );
 
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error :(</p>;
-  console.log(data.hello);
-
-  return <div className="App">{data ? data.hello : null}</div>;
+  return (
+    <div className="App">
+      <div>{qdata ? qdata.hello : null}</div>
+      <div>{sdata ? sdata.time : null}</div>
+    </div>
+  );
 }
 
 export default App;
--- web/src/index.js
+++ web/src/index.js
@@ -3,9 +3,35 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import { split, HttpLink } from "@apollo/client";
+import { getMainDefinition } from "@apollo/client/utilities";
+import { WebSocketLink } from "@apollo/client/link/ws";
+
+const httpLink = new HttpLink({
+  uri: "http://localhost:4000/graphql",
+});
+
+const wsLink = new WebSocketLink({
+  uri: `ws://localhost:4000/graphql`,
+  options: {
+    reconnect: true,
+  },
+});
+
+const link = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === "OperationDefinition" &&
+      definition.operation === "subscription"
+    );
+  },
+  wsLink,
+  httpLink
+);
 
 const client = new ApolloClient({
-  uri: "http://localhost:4000/graphql",
+  link,
   cache: new InMemoryCache(),
 });
 
